74e53dbe1c599a560ec3273b552c8a4c0a636e0b
binny <binny0x00@gmail.com>
2026-05-05 03:21:28 +0900

    style: process 포맷 수정

--- pintos/include/userprog/process.h
+++ pintos/include/userprog/process.h
@@ -3,19 +3,20 @@
 
 #include "threads/thread.h"
 
-tid_t process_create_initd (const char *file_name);
-tid_t process_fork (const char *name, struct intr_frame *if_);
-int process_exec (void *f_name);
-int process_wait (tid_t);
-void process_exit (void);
-void process_activate (struct thread *next);
+tid_t process_create_initd(const char *file_name);
+tid_t process_fork(const char *name, struct intr_frame *if_);
+int process_exec(void *f_name);
+int process_wait(tid_t);
+void process_exit(void);
+void process_activate(struct thread *next);
 
 /* 파일 디스크립터 헬퍼 */
-int process_add_file (struct file *f);
-struct file *process_get_file (int fd);
-void process_close_file (int fd);
+int process_add_file(struct file *f);
+struct file *process_get_file(int fd);
+void process_close_file(int fd);
 
-struct fork_args {
+struct fork_args
+{
     struct intr_frame *if_;
     tid_t current_tid;
 };
